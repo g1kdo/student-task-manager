a95258a97e35f4f77f682a3a79166574dac88fe7
g1kdo <july.gassy-8k@icloud.com>
2026-07-06 18:00:57 +0200

    First commit: initial project setup

---
+++ src/java/Task.java
@@ -0,0 +1,24 @@
+package java;
+
+public class Task {
+
+    private String name;
+    private boolean completed;
+
+    public Task(String name) {
+        this.name = name;
+        this.completed = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void complete() {
+        completed = true;
+    }
+}
